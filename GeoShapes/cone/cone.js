0e738f61e020732a8a5530b270f5f61cbfebae1c
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-11-08 23:07:32 +0500

    Fixing a bug

--- GeoShapes/cone/cone.js
+++ GeoShapes/cone/cone.js
@@ -3,7 +3,6 @@ const r = document.querySelector('#radius').value
 const l = document.querySelector('#length').value
 const h = document.querySelector('#height').value
 const data = document.querySelector('#data')
-const calc = document.querySelector('#calc')
 const result = document.querySelector('#result')
 let v = document.querySelector('#v');
 let s = document.querySelector('#s');
