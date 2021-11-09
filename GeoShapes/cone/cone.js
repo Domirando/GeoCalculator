4167ae27a0e8347d3f667772f69b2c99717eb3ec
MaisieDev <vohidjonovnamaftuna@gmail.com>
2021-11-09 16:26:44 +0500

    Fixing bugs

--- GeoShapes/cone/cone.js
+++ GeoShapes/cone/cone.js
@@ -1,20 +1,33 @@
+const calc = document.querySelector("#calc");
+const back = document.querySelector("#back");
 const pi = 3.14;
-const r = document.querySelector('#radius').value
-console.log(r)
-const l = document.querySelector('#length').value
-console.log(l)
-const h = document.querySelector('#height').value
-const data = document.querySelector('#data')
-const calc = document.querySelector('#calc')
-const result = document.querySelector('#result')
-let v = document.querySelector('#v').innerHTML;
-let s = document.querySelector('#s').innerHTML;
 
-v+=(pi*(h/3)*Math.pow(r, 2))
-s+=(pi*r*(r+l))
-console.log(v+"; "+s)
-    
-calc.addEventListener('click', ()=>{
-    data.style.display="none";
-    result.style.display="block";          
-})
+calc.addEventListener("click", (e) => {
+  e.preventDefault();
+  Calculate();
+  data.style.display = "none";
+  result.style.display = "block";
+});
+
+function Calculate() {
+  const r = document.querySelector("#radius").value;
+  console.log(r);
+  const l = document.querySelector("#length").value;
+  console.log(l);
+  const h = document.querySelector("#height").value;
+  const data = document.querySelector("#data");
+  const result = document.querySelector("#result");
+
+  let v = pi * (h / 3) * Math.pow(r, 2);
+  let s = pi * r * (r + l);
+
+  document.querySelector("#v").innerHTML =
+    document.querySelector("#v").innerHTML + v;
+  document.querySelector("#s").innerHTML =
+    document.querySelector("#s").innerHTML + s;
+}
+
+back.addEventListener("click", (e) => {
+  data.style.display = "block";
+  result.style.display = "none";
+});
